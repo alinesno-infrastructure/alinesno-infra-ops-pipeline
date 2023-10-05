1c3708c84798eaf44ac5f98f9142b5b360e36319
CarrotSea233 <2733614757@qq.com>
2023-10-05 15:26:45 +0800

    feat: 1.集成UI，2.集成SSO认证

--- alinesno-infra-ops-pipeline-boot/src/main/java/com/alinesno/infra/ops/pipeline/OpsPipelineApplication.java
+++ alinesno-infra-ops-pipeline-boot/src/main/java/com/alinesno/infra/ops/pipeline/OpsPipelineApplication.java
@@ -1,6 +1,7 @@
 package com.alinesno.infra.ops.pipeline;
 
 import com.alinesno.infra.common.core.context.SpringContext;
+import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Bean;
  * @author luoxiaodong
  * @version 1.0.0
  */
+@EnableInfraSsoApi
 @MapperScan("com.alinesno.infra.ops.pipeline.mapper")
 @SpringBootApplication
 public class OpsPipelineApplication {
